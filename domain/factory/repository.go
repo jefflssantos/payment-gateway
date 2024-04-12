1e3c540f8a1523b33a590672249f7e78aad1960a
jefflssantos <email@jeffsantos.com.br>
2024-04-12 15:50:09 -0300

    Update go version and workspace path

--- domain/factory/repository.go
+++ domain/factory/repository.go
@@ -1,6 +1,6 @@
 package factory
 
-import "github.com/jefflssantos/imersao/gateway/domain/repository"
+import "github.com/jefflssantos/payment-gateway/domain/repository"
 
 type RepositoryFactory interface {
 	CreateTransactionRepository() repository.TransactionRepository
